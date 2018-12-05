b106df7328064dcfd21dd8f07ee997bfd164e901
Stuardo -StR- Rodríguez <str@maphpia.com>
2018-12-05 16:23:55 -0600

    Adding documentation

--- prime.php
+++ prime.php
@@ -4,15 +4,29 @@ namespace Maphpia\Demos;
 /**
  * Which prime below a million can be written as the sum of the most consecutive primes
  */
-
 class Primes
 {
     const MAX = 1 *1000 *1000; // one million
 
+    /**
+     * Sum of the primes
+     *
+     * @var int
+     */
     private $sum;
+
+    /**
+     * Calculated primes
+     *
+     * @var int[]
+     */
     private $primes;
 
-
+    /**
+     * Calculates the primes and pushes them in an array.
+     *
+     * @return void
+     */
     private function calculate()
     {
         $sum    = 0;
@@ -35,17 +49,30 @@ class Primes
 
     }
 
+    /**
+     * Output of the primes and their sum.
+     *
+     * @return void
+     */
     private function show()
     {
         echo implode(' + ', $this->primes) . " = {$this->sum}";
         echo "\n";
     }
 
+    /**
+     * Initializes the program detects dependencies
+     */
     public function __construct()
     {
         if (!function_exists('gmp_nextprime')) throw new Exception('reqquired GMP library not installed');
     }
 
+    /**
+     * Public method to run the project
+     *
+     * @return void
+     */
     public function run()
     {
         $this->calculate();
